ae654508ae5dcbfe4a1268e44a6e3930d6ab4860
Khaled <k.e.elsayed@gmail.com>
2021-06-15 22:31:41 -0700

    Better command line

--- Mandoline/bytecode-gen/MandolineLogger.java
+++ Mandoline/bytecode-gen/MandolineLogger.java
@@ -10,23 +10,36 @@ public class MandolineLogger{
         Runtime.getRuntime().addShutdownHook(new MandolineShutdown());
     }
 
-    // public static synchronized void println(String e) {
-    //     System.out.println("SLICING:"+e);
-    // }
-
-    public static synchronized void println(String e) {
-        queue.add(e);
-        if (queue.size() > SIZE) {
-            flush();
-            // mw.start();
+    public static void println(String e) {
+        synchronized (queue) {
+            System.out.println("SLICING:"+e);
         }
     }
+
+    // public static void println(String e) {
+    //     synchronized (queue) {
+    //         queue.add(e);
+    //         if (queue.size() > SIZE) {
+    //             flush();
+    //             // mw.start();
+    //         }
+    //     }
+    // }
     
-    public static synchronized void flush() {
-        outStream.println("Flushing queue");
-        for (String s: queue) {
-            outStream.println("SLICING:"+s);
+    public static void flush(String e) {
+        synchronized (queue) {
+            flush();
+            outStream.println("SLICING:"+e);
         }
-        queue.clear();
+    }
+
+    public static void flush() {
+        synchronized (queue) {
+            outStream.println("Flushing queue");
+            for (String s: queue) {
+                outStream.println("SLICING:"+s);
+            }
+            queue.clear();
+        }
     }
 }
