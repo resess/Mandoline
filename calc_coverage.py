b4c4ba5a5f753ce64ee7684da1c43e1fa7d9dc85
Khaled E. Ahmed <k.e.elsayed@gmail.com>
2022-12-06 13:35:12 -0800

    code: calc coverage

--- calc_coverage.py
+++ calc_coverage.py
@@ -1,20 +1,68 @@
 import sys
+import json
 
-in_file = sys.argv[1]
+trace_file = sys.argv[1]
+static_log_file = sys.argv[2]
 
-all_stmts = dict()
-with open(in_file, 'r') as f:
+all_bytecode_stmts = dict()
+all_java_stmts = dict()
+old_java_stmt = ""
+with open(trace_file, 'r') as f:
     for line in f:
-        method, stmt = line.split(", ")[1:3]
-        bytecode_stmt = method  + stmt
-        if bytecode_stmt not in all_stmts:
-            all_stmts[bytecode_stmt] =0
-        all_stmts[bytecode_stmt] = all_stmts[bytecode_stmt] + 1
+        method, clazz, stmt = line.split(", ")[1:4]
+        method = method.replace(";", ",")
+        # print(stmt)
+        stmt, line_no = stmt.split(":LINENO:")[0:2]
+        line_no = line_no.split(":")[0]
+        bytecode_stmt = "<" + clazz + ": " + method + ">" + ":" + stmt
+        java_stmt = "<" + clazz + ": " + method + ">" + ":" + line_no
 
-print("Exec stmts")
-print(len(all_stmts))
+        # print(java_stmt)
+        # print(bytecode_stmt)
+        if bytecode_stmt not in all_bytecode_stmts:
+            all_bytecode_stmts[bytecode_stmt] = 0
+        all_bytecode_stmts[bytecode_stmt] = all_bytecode_stmts[bytecode_stmt] + 1
 
-print("Exec stmt instances")
-print(sum(all_stmts.values()))
+        if java_stmt not in all_java_stmts:
+            all_java_stmts[java_stmt] = 0
+        if old_java_stmt != java_stmt:
+            all_java_stmts[java_stmt] = all_java_stmts[java_stmt] + 1
+
+        old_java_stmt = java_stmt
+
+print("Exec bytecode stmts")
+print(len(all_bytecode_stmts))
+
+print("Exec bytecode stmt instances")
+print(sum(all_bytecode_stmts.values()))
+
+print("Exec java stmts")
+print(len(all_java_stmts))
+
+print("Exec java stmt instances")
+print(sum(all_java_stmts.values()))
 
 
+with open(static_log_file, 'r') as f:
+    data = json.load(f)
+
+# print(data)
+
+all_possible_bytecode_stmts = set()
+all_possible_java_stmts = set()
+
+for m in data:
+    # print("-------------")
+    # print(m)
+    for l in data[m]:
+        java_stmt = m + ":" + l
+        all_possible_java_stmts.add(java_stmt)
+        for b in data[m][l]:
+            bytecode_stmt = m + ":" + b
+            all_possible_bytecode_stmts.add(bytecode_stmt)
+
+print("Bytecode stmts coverage")
+print(100*len(all_bytecode_stmts)/len(all_possible_bytecode_stmts))
+
+print("Java stmts coverage")
+print(100*len(all_java_stmts)/len(all_possible_java_stmts))
